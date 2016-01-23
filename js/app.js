8768cc5b927a82766bf7b4c50145a13130b2727d
dkmullen <dkmullen@gmail.com>
2016-01-23 13:06:28 -0500

    Put map marker creation into a loop

--- js/app.js
+++ js/app.js
@@ -1,33 +1,49 @@
 var map;
+var markers = [
+	{position: {lat: 35.883324, lng: -84.523895},
+		map: map,
+		title: "Mama Mia's Restaurant-Pizzeria"
+	},
+	{position: {lat: 35.8833019, lng: -84.52331630000003},
+		map: map,
+		title: "Smokehouse Bar n Grill"
+	},
+	{position: {lat: 35.8776972, lng: -84.5229296},
+		map: map,
+		title: "RedBones on the River"
+	},
+	{position: {lat: 35.875116, lng: -84.52033013105392},
+		map: map,
+		title: "Roane County High School"
+	},
+	{position: {lat: 35.874415, lng: -84.515031},
+		map: map,
+		title: "Handee Burger"
+	},
+	{position: {lat: 35.876639, lng: -84.521292},
+		map: map,
+		title: "Kingston Community Center"
+	}
+];
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 35.8796114, lng: -84.5159483},
     zoom: 16
   });
   
-  var marker1 = new google.maps.Marker({
-    position: {lat: 35.883324, lng: -84.523895},
-    map: map,
-    title: "Mama Mia's Restaurant-Pizzeria"
-  });
-  var marker2 = new google.maps.Marker({
-    position: {lat: 35.8833019, lng: -84.52331630000003},
-    map: map,
-    title: "Smokehouse Bar n Grill"
-  });
-  var marker3 = new google.maps.Marker({
-    position: {lat: 35.8776972, lng: -84.5229296},
-    map: map,
-    title: "RedBones on the River"
-  });
-  var marker4 = new google.maps.Marker({
-    position: {lat: 35.875116 , lng: -84.52033013105392},
-    map: map,
-    title: "Roane County High School"
-  });
-  var marker5 = new google.maps.Marker({
-    position: {lat: 35.874415  , lng: -84.515031},
-    map: map,
-    title: "Handee Burger"
-  });
+for (i = 0; i < markers.length; i++) {
+	var marker = new google.maps.Marker({
+		position: markers[i].position,
+		map: map,
+		title: markers[i].title
+	});
+};
+
 }
+//TODO - Get the map into the view model
+var ViewModel = function() {
+
+};
+ko.applyBindings(new ViewModel())
+
